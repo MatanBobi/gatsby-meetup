f01066623ee95486d4035823674418229ccbdae9
Matan Borenkraout <matanbobi@gmail.com>
2020-07-07 22:37:03 +0300

    Project structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <h1>Home page!</h1>
+      <Link to="/example">Take me to example page</Link>
+    </>
+  )
 }
