79ad4550e6f6addd3b235e493adcabf3174b7807
Matan Borenkraout <matanbobi@gmail.com>
2020-07-08 22:21:25 +0300

    Use siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,10 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: "Gatsby meetup",
+    description: "This is the site built from the Gatsby meetup",
+  },
   /* Your site config here */
   plugins: ["gatsby-plugin-typography", "gatsby-plugin-styled-components"],
 }
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,22 @@
 import React from "react"
 import "../../styles/index.css"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 export default ({ pageHeader, children }) => {
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+  `)
+
   return (
     <>
-      <div className="app-header">My header</div>
+      <div className="app-header">{data.site.siteMetadata.title}</div>
       <div className="page-container">
         <h1 className="header">{pageHeader}</h1>
         <aside className="sidebar">
